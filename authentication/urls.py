76e3813d2ebbd08874de786cc187cfcf566d529d
Jirat Charoenkaew <jirat8098@gmail.com>
2025-03-01 16:08:54 +0700

    Mock up admin page - frontend

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), 
+    path('admin-system/users_management/', views.users_management, name='users_management'),
+    path('admin-system/dashboard/', views.dashboard, name='dashboard'),
+    path('admin-system/solar_management/', views.solar, name='solar_management'),
+    path('admin-system/upload_history/', views.upload, name='upload_history'),
+    path('admin-system/reports/', views.reports, name='reports'),
 ]
