811eb5bf6432375f5b772d74f4848381ade04c08
Jirat Charoenkaew <jirat8098@gmail.com>
2025-03-01 19:34:20 +0700

    App admin app in settings.py and include their urls

--- admin/urls.py
+++ admin/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin-system/users_management/', views.users_management, name='users_management'),
-    path('admin-system/dashboard/', views.dashboard, name='dashboard'),
-    path('admin-system/solar_management/', views.solar, name='solar_management'),
-    path('admin-system/upload_history/', views.upload, name='upload_history'),
-    path('admin-system/reports/', views.reports, name='reports'),
+    path('users_management/', views.users_management, name='users_management'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('solar_management/', views.solar, name='solar_management'),
+    path('upload_history/', views.upload, name='upload_history'),
+    path('reports/', views.reports, name='reports'),
 ]
