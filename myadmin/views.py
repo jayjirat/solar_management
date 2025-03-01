4ea9de31f5736de685acb2c5e4f1308f5eaf35ff
Jirat Charoenkaew <jirat8098@gmail.com>
2025-03-01 19:55:12 +0700

    Edit file path and add logout button

--- myadmin/views.py
+++ myadmin/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def users_management(request):
-    return render(request, 'admin/users_management.html')
+    return render(request, 'users_management.html')
 
 def dashboard(request):
-    return render(request, 'admin/dashboard.html')
+    return render(request, 'dashboard.html')
 
 def solar(request):
-    return render(request, 'admin/solar_management.html')
+    return render(request, 'solar_management.html')
 
 def upload(request):
-    return render(request, 'admin/upload_history.html')
+    return render(request, 'upload_history.html')
 
 def reports(request):
-    return render(request, 'admin/reports.html')
+    return render(request, 'reports.html')
