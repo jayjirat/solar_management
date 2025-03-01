ebcf50a915c061c9a9e652abd3997e6267b0ba21
Parathakorn Thanamee <6510615187@cn330>
2025-03-01 15:08:51 +0700

    Change Urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('login/google/callback/', views.google_callback, name='google_callback'),
     path('login/facebook/', views.facebook_login, name='facebook_login'),  
     path('login/facebook/callback/', views.facebook_callback, name='facebook_callback'),  
-    path("login/tu/", ThammasatLoginAPI.as_view(), name="tu-login"),
+    path("api/login/tu/", ThammasatLoginAPI.as_view(), name="tu-login"),
 ]
