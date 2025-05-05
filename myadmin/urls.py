f29d0d3c739911f6c881894bec30e59776fa7a80
Jirat Charoenkaew <jirat8098@gmail.com>
2025-05-05 15:01:08 +0700

    User management status and role

--- myadmin/urls.py
+++ myadmin/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('users_management/', views.users_management, name='users_management'),
+    path('users_management/<int:user_id>', views.users_management_manage, name='users_management_manage'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('solar_management/', views.solar, name='solar_management'),
     path('upload_history/', views.upload, name='upload_history'),
--- myadmin/views.py
+++ myadmin/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from authentication.models import CustomUser
 # Create your views here.
 
@@ -8,6 +8,20 @@ def users_management(request):
     context = {'users': users}
     return render(request, 'users_management.html',context=context)
 
+def users_management_manage(request,user_id):
+    customuser = CustomUser.objects.get(id=user_id)
+
+    if request.method == 'POST':
+        customuser.role = request.POST.get('role')
+        customuser.status = request.POST.get('status')
+        customuser.save()
+        return redirect('users_management')
+
+    return render(request, 'users_management_manage.html', {
+        'customuser': customuser,
+        'roles': [('admin', 'Admin'), ('data_analyst', 'Data Analyst'),('drone_controller', 'Drone Controller')],
+        'statuses': [('active', 'Active'), ('inactive', 'Inactive')],
+    })
 
 def dashboard(request):
     return render(request, 'dashboard.html')
